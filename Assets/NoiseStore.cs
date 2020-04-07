de1eb9d6b8469f343ecb73cbe19c48cd19124f21
Alwin Hughes <alwin.hughes@btopenworld.com>
2020-04-07 00:58:13 +0000

    can visualise basic 2d noise stores

---
+++ Assets/NoiseStore.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CreateAssetMenu()]
+[Serializable]
+public class NoiseStore : ScriptableObject {
+
+  [SerializeField]
+  private float[] store;
+
+  [SerializeField]
+  private int[] dims;
+
+  [SerializeField]
+  private int rank;
+
+  [SerializeField]
+  private int[] dims_mul;
+
+  public NoiseStore(){}
+
+  public NoiseStore(int[] dims) {
+    this.dims = dims;
+    this.store = new float[storeLength()];
+    rank = dims.Length;
+    computeDimsMul();
+  }
+
+  private void computeDimsMul() {
+    dims_mul = new int[dims.Length];
+    int acc = 1;
+    for(int i = 0; i < dims_mul.Length; i++) {
+      dims_mul[i] = acc;
+      acc *= dims[i];
+    }
+  }
+
+  public void zero() {
+    this.store = new float[storeLength()];
+  }
+
+  public int storeLength() {
+    int ret = 1;
+    for(int i = 0; i < dims.Length; i++) {
+      ret *= dims[i];
+    }
+    return ret;
+  }
+
+  public int[] getDims() { return dims; }
+
+  public int getStoreIndex(int[] item) {
+    if(item.Length != dims.Length) {
+      throw new Exception("dimention mismatch");
+    }
+
+    int ret = 0; 
+    for(int i = 0; i < item.Length; i++) {
+      ret += item[i] * dims_mul[i];
+    }
+    return ret;
+  }
+
+  public float get(int[] index) {
+    return store[getStoreIndex(index)];
+  }
+
+  public void set(int[] index, float val) {
+    store[getStoreIndex(index)] = val;
+  }
+
+  public bool isReady() {
+    return store != null;
+  }
+
+  public float getMin() {
+    float min = store[0];
+    for(int i = 0; i < store.Length; i++) {
+      if(store[i] < min) {
+        min = store[i];
+      }
+    }
+    return min;
+  }
+
+  public float getMax() {
+    float max = store[0];
+    for(int i = 0; i < store.Length; i++) {
+      if(store[i] > max) {
+        max = store[i];
+      }
+    }
+    return max;
+  }
+
+}
---
+++ Assets/NoiseViewerEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NoiseViewer))]
+public class NoiseViewerEditor : Editor {
+
+  private bool ns_fold_out = true;
+  private Editor ns_editor;
+
+  public override void OnInspectorGUI() {
+    DrawDefaultInspector();
+
+    NoiseViewer nv = (NoiseViewer) target;
+
+    DrawSettingsEditor(nv.noise_store, nv.onNoiseStoreChange, ref ns_fold_out, ref ns_editor);
+
+  }
+
+  void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated, ref bool foldout, ref Editor editor)
+  {
+    if (settings != null)
+    {
+      foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+      using (var check = new EditorGUI.ChangeCheckScope())
+      {
+        if (foldout)
+        {
+          CreateCachedEditor(settings, null, ref editor);
+          editor.OnInspectorGUI();
+
+          if (check.changed)
+          {
+            if (onSettingsUpdated != null)
+            {
+              onSettingsUpdated();
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
